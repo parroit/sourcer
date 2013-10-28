9cd8f4ea7fb99a666ce226ef17d7d3216a5c77a5
parroit <andrea.parodi@ebansoftware.net>
2013-10-28 22:04:58 +0100

    js mode hint

--- js/document-view.js
+++ js/document-view.js
@@ -1,41 +1,88 @@
 var events = require("events");
 var uuid = require('node-uuid');
-function DocumentView(editor, tabsElm, $,CodeMirror){
+var _ = require('lodash');
+function DocumentView(editor, tabsElm, $, CodeMirror) {
     this.events = new events.EventEmitter();
     this.editor = editor;
     this.emptyDoc = editor.getDoc();
-    this.tabsElm=tabsElm;
-    this.$=$;
-    this.CodeMirror=CodeMirror;
+    this.tabsElm = tabsElm;
+    this.$ = $;
+    this.CodeMirror = CodeMirror;
 
 }
 
 module.exports = DocumentView;
 
-DocumentView.prototype.setDocumentCtrl = function(ctrl){
+DocumentView.prototype.setDocumentCtrl = function (ctrl) {
     var self = this;
     var $ = self.$;
 
-    function onCaptionChanged () {
+    function onCaptionChanged() {
 
-        self.anchor.html(ctrl.caption);
+        $("#" + tab.attr("id")+ " a").html(ctrl.caption);
     }
 
-    if (self.ctrl){
+    if (self.ctrl) {
         self.ctrl.events.removeListener('captionChanged', onCaptionChanged);
     }
     self.ctrl = ctrl;
 
-    if (self.ctrl){
+    if (self.ctrl) {
         self.ctrl.events.on('captionChanged', onCaptionChanged);
-        //self.editor.setOption("mode", ctrl.mimeType);
 
-        self.CodeMirror.requireMode(ctrl.mode, function(){
+        if (ctrl.mode) {
+            var modeName;
+            var extraKeys;
+            var extraCommands;
+            if (_.isString(ctrl.mode))
+            {
+                modeName = ctrl.mode;
+                extraKeys= {};
+                extraCommands ={};
+            }
+            else
+            {
+                modeName = ctrl.mode.name;
+                extraKeys= ctrl.mode.extraKeys || {};
+                extraCommands = ctrl.mode.extraCommands || {};
+                if (ctrl.mode.plugins)
+                for (var i= 0,l=ctrl.mode.plugins.length; i<l; i++){
+                    self.$.getScript(ctrl.mode.plugins[i], function( data, textStatus, jqxhr ) {
+                        console.log( data ); // Data returned
+                        console.log( textStatus ); // Success
+                        console.log( jqxhr.status ); // 200
+                        console.log( "Load was performed." );
+                    });
+                }
+
+                for (var i= 0,l=ctrl.mode.styles.length; i<l; i++){
+                    $('<link>')
+                        .appendTo($('head'))
+                        .attr({type : 'text/css', rel : 'stylesheet'})
+                        .attr('href', ctrl.mode.styles[i]);
+
+
+                }
+            }
+
+
+            self.CodeMirror.requireMode(modeName, function () {
+                for (var k in extraCommands) {
+                    var factory= eval("("+extraCommands[k]+")");
+                    self.CodeMirror.commands[k] = factory(self.CodeMirror);
+                }
+
+                self.CodeMirror.keyMap.default = _.extend(self.CodeMirror.keyMap.default,extraKeys);
+
+
+
+                self.editor.swapDoc(ctrl.doc);
+
+            });
+        } else {
+
             self.editor.swapDoc(ctrl.doc);
-        });
-
-
-
+        }
 
 
         var fileId = ctrl.filepath.replace(/[\\\/ :.-_]/g, "X");
@@ -43,7 +90,7 @@ DocumentView.prototype.setDocumentCtrl = function(ctrl){
 
         var anchor;
         var tab;
-        tab = $("#"+fileId);
+        tab = $("#" + fileId);
 
         function createNewTab() {
             anchor = $("<a>");
@@ -52,9 +99,9 @@ DocumentView.prototype.setDocumentCtrl = function(ctrl){
             anchor.attr("href", "");
             anchor.attr("data-path", ctrl.filepath);
             anchor.html(ctrl.caption);
-            anchor.click(function(){
+            anchor.click(function () {
                 $(this).attr("data-path");
-                self.events.emit('requestDocumentActivation',$(this).attr("data-path"));
+                self.events.emit('requestDocumentActivation', $(this).attr("data-path"));
             });
 
             tab = $("<li>");
@@ -66,20 +113,20 @@ DocumentView.prototype.setDocumentCtrl = function(ctrl){
             self.tabsElm.append(tab);
 
 
-            ctrl.events.on('documentClosed',function(){
+            ctrl.events.on('documentClosed', function () {
                 $("#" + tab.attr("id")).remove();
 
             });
 
-            self.anchor = tab.find("a");
+            //self.anchor = tab.find("a");
 
             tab = $("#" + tab.attr("id"));
         }
 
-        if (tab.length){
-            self.anchor = tab.find("a");
+        if (!tab.length) {
+            //self.anchor = tab.find("a");
 
-        } else {
+        //} else {
             createNewTab();
         }
         self.tabsElm.find(".uk-tab-responsive").remove();
