2607e9e65b35da844ecc578133c05d373947fcc7
parroit <andrea.parodi@ebansoftware.net>
2013-12-19 21:59:28 +0100

    editor resize

--- core/view/js/index.js
+++ core/view/js/index.js
@@ -1,41 +1,16 @@
 (function(){
-    $(document).ready( function() {
 
-        // TABS-WEST - sortable
-        $(".ui-layout-west")
-            .tabs()
-            .find(".ui-tabs-nav")
-            .sortable({ axis: 'x', zIndex: 2 })
-        ;
-
-        // TABS-EAST - sortable
-        $(".ui-layout-east")
-            .tabs()
-            .find(".ui-tabs-nav")
-            .sortable({ axis: 'x', zIndex: 2 })
-        ;
-
-        // TABS-CENTER - sortable
-        $(".ui-layout-center")
-            .tabs({
-                change: function () {  }
-            })
-            .find(".ui-tabs-nav")
-            .sortable({ axis: 'x', zIndex: 2 })
-        ;
-
-        // PAGE LAYOUT
-        var myLayout = $('body').layout();
-
-
-
-    });
 
     var app = require("../../lib/app"),
         gui = app.views.vendor.gui = require('nw.gui');
 
     app.views.vendor.$ = $;
+    app.views.vendor.ace = ace;
+
     app.views.window = gui.Window.get();
+    app.views.htmlWindow = window;
+    app.views.document = document;
+
     app.init();
 
 
